f69774bdfa4ea01e95575ce9a1d5c514704d884e
tateorrtot <tatediscord@gmail.com>
2026-03-11 22:34:23 -0700

    Added pausing with P

--- addons/foxbox/characters/demo/gui.gd
+++ addons/foxbox/characters/demo/gui.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("pause"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			pause()
 			
